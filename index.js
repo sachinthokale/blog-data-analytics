b5e2e03cc74f960dafaf5a2700d95c386f0baf63
Sachin Thokale <sachinthokale999@gmail.com>
2023-10-09 20:54:24 +0530

    second commit

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express, { application } from "express";
 import dotenv from "dotenv";
 import { router } from "./routes/blogRoutes.js";
 
+// Load environment variables from a .env file into the process's environment.
 dotenv.config();
 
 const app = express();
+// Middleware for parsing URL-encoded data with extended options and a file size limit of 50MB
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 app.use(express.json()); // to accept json data
 app.use("/api", router);
 
